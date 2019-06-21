c675bc039e7acc7acb09881c1108d22cb1aca897
Ashley Tolen <tolenashley7@gmail.com>
2019-06-21 00:00:16 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,23 @@
+# cool_nums = [1, 2, 3, 4, 5]
+ 
+# def even_nums(nums)
+#   nums.select do |x|
+#     x.even?
+#   end
+# end
+ 
+# even_nums(cool_nums)
+# #=> [2, 4]
+
 def my_select(collection)
- # your code here!
-end
+ i = 0
+ new_collection = []
+ 
+ while i < collection.length
+  new_collection << yield(collection[i])
+  i = i + 1
+ end
+ 
+ # how do I return just collection true
+ new_collection[i][true]
+end 
